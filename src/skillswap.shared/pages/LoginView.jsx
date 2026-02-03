1826f8ae4fba3d16117fc717670efc78996f2a57
Adrian Puchalski <8adbamdom@gmail.com>
2026-02-03 23:52:40 +0100

    fix: fix navigate to app after logged in;

--- src/skillswap.shared/pages/LoginView.jsx
+++ src/skillswap.shared/pages/LoginView.jsx
@@ -19,7 +19,7 @@ export function LoginView() {
             sameSite: 'strict'
         });
 
-        window.location.replace('/app');
+        navigate('/app', { replace: true }); 
     };
 
     return (
